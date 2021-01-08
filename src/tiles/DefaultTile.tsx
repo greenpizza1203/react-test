ccdd478a8c9a4221daaf7fcda0099541df7afdd0
greenpizza1203 <konakarthik12@gmail.com>
2021-01-08 15:41:39 -0500

    changes

--- src/tiles/DefaultTile.tsx
+++ src/tiles/DefaultTile.tsx
@@ -1,12 +1,12 @@
 import {ITile} from "monocommon";
 import React from "react";
-
+import * as styles from "../../css/board/tile.module.scss"
 
 export abstract class DefaultTile extends React.Component<{ info: ITile }> {
 
     render() {
         return (
-            <div className="tile">
+            <div className={styles.tile}>
                 {this.getTitle()}
             </div>
         )
